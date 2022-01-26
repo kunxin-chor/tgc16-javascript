4a20894552b01a92bddce020d82803e30237d0a4
kunxin-chor <chorkunxin@yahoo.com>
2022-01-26 03:22:33 +0000

    Added variables and data type example

---
+++ 01-variables/variables.js
@@ -0,0 +1,39 @@
+// Data types in JavaScript
+// NUMBERS
+// integers
+// floats
+// 
+// strings 
+// booleans
+// aka primitive data types
+
+// integer
+42
+
+// strings --> text
+"she sells seashell at the seashore"
+'jack and jill went up the hills'
+'asd1234!'
+
+// boolean values
+// true or false
+
+// Variables
+// Variables basically stored values
+
+// create a js variable
+let secretOfLife = 42;
+console.log(secretOfLife);
+
+// imagine console to be a container that can have
+// many functions, and log is one of those functions
+// the dot means 'belongs to'.
+// think of the . as the 's
+// console.log ==> console's log function
+
+// you can `let` a variable once (*normally)
+// let secretOfLife = 43; ==> ERROR! Cannot redeclare variables
+
+// you can reassign new values to an existing variable
+secretOfLife = 43;
+console.log("secretOfLife =", secretOfLife);
