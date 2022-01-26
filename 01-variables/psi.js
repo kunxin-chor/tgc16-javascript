18d8b5b67706af93e39741362c805365c62c9ccd
kunxin-chor <chorkunxin@yahoo.com>
2022-01-26 06:51:18 +0000

    Added warning message

--- 01-variables/psi.js
+++ 01-variables/psi.js
@@ -3,7 +3,8 @@ const prompt = require('prompt-sync')();
 let psi = parseFloat(prompt("Please enter the psi: "));
 let quality = 0;
 if (psi <= 150) {
-    let quality = "Healthy";
+    let quality = "Healthy"; // WARNING! The quality variable here
+                             // is not available at line 13!
 } else if (psi <= 300) {
     let quality = "Unhealthy";
 } else {
