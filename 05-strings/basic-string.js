b3a0e6d04eb9f8137b6240ae8940683c4a08008d
kunxin-chor <chorkunxin@yahoo.com>
2022-01-28 02:36:45 +0000

    Added basic string and escape sequence

---
+++ 05-strings/basic-string.js
@@ -0,0 +1,11 @@
+// strings begin with a ' or "
+let words = "the quick brown fox jumps over the lazy dog";
+let rhyme = 'she sells seashells at the seashore';
+let paragraph = "don't kill me I don't know anything";
+let story = 'she said, "I sells seashells at the seashore"';
+let story2 = 'She said, "I don\'t know anything"';
+let story3 = "He said, \"gong xi fa cai\"";
+console.log("A\nB");
+console.log("Social\tDistancing");
+console.log("I wish you a merry christmas\nAnd a happy new year\nfor 2022");
+console.log("C:\\documents\\tests");
