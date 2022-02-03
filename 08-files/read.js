876547cf6b417d4703ea202c645d2d25d78a5d4b
kunxin-chor <chorkunxin@yahoo.com>
2022-02-03 05:56:58 +0000

    Added read from file example

---
+++ 08-files/read.js
@@ -0,0 +1,13 @@
+// for our NodeJS program to do file i/o (read data and write data via files)
+// we need the file system module ('fs' for short)
+// the fs module is already part of NodeJS so we don't need to yarn add
+const fs = require('fs');
+
+// open the file to read in the data
+// the sync at back of readFileSync means JavaScript
+// wait till the file has finished reading.
+let data = fs.readFileSync('data.txt');
+
+// convert the raw data to string
+let text = data.toString();
+console.log(text);
